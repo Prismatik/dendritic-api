c280a11a52b0463a17ce50eec45b0db5977f63bd
Nikolay Nemshilov <nemshilov@gmail.com>
2016-09-07 15:04:35 +1000

    Ditching the legacy generators code

--- legacy/lib/captains_log.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-exports = module.exports = (argv) => {
-  const arg = argv.slice(2);
-  arg.unshift('redbeard');
-  const str = `${arg.join(' ')}\n`;
-  fs.appendFileSync('./captains_log', str);
-};
--- legacy/lib/package.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const _ = require('lodash');
-
-// add dependencies to an already generated package.json file
-exports.addDependencies = function addDependencies(packagePath, newDependencies) {
-  const pkg = JSON.parse(fs.readFileSync(packagePath));
-  const updated = _.merge(pkg, { dependencies: newDependencies });
-  fs.writeFileSync(packagePath, JSON.stringify(updated, null, 2));
-};
